06a21754bf618aaadf4ec924c7abb1dae078a03b
rivasd <rivasdaniel1992@gmail.com>
2017-10-10 15:52:09 -0400

    added new launch config using the sample BDF file from Biosemi

--- loader.py
+++ loader.py
@@ -1,7 +1,15 @@
-import keras
 import argparse
+import keras
+import pyedflib
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="full path to the BDF file you wish to read and learn from")
 args = parser.parse_args()
+
+# load the specified file into memory
+#TODO: might be too large? find way to load in steps or distributed
+
+bdfData = pyedflib.EdfReader(args.file)
+n = bdfData.signals_in_file
+
 
